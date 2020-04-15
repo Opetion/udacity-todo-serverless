eb9c75115750899e47fe0ef502f32af5f6c8aa13
Opetion <jfc.costa91@gmail.com>
2020-04-15 14:48:42 +0100

    :wrench: add auth0 config and aws API id

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,8 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'jus9br52y1'
+export const apiEndpoint = `https://${apiId}.execute-api.eu-west-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'opetion.eu.auth0.com',            // Auth0 domain
+  clientId: 'sv2sNplCb3Wx4Kuu8PN63EPKcmaXZ8fI',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
