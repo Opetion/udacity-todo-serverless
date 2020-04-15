46c61aee23cb143e74d183e13831813315eaf9f2
Opetion <jfc.costa91@gmail.com>
2020-04-15 14:52:35 +0100

    :sparkles: add jwt helper

--- backend/src/auth/utils.ts
+++ backend/src/auth/utils.ts
@@ -3,11 +3,36 @@ import { decode } from 'jsonwebtoken'
 import { JwtPayload } from './JwtPayload'
 
 /**
+ * Parse a JWT token from the "Authorization" header and return a user id
+ * @param header content of the header "Authorization"
+ * @returns a user id from the JWT token
+ */
+export function parseUserIdHeader(header: string): string {
+  const token = getToken(header);
+  return parseUserId(token);
+}
+
+/**
  * Parse a JWT token and return a user id
  * @param jwtToken JWT token to parse
  * @returns a user id from the JWT token
  */
 export function parseUserId(jwtToken: string): string {
-  const decodedJwt = decode(jwtToken) as JwtPayload
-  return decodedJwt.sub
+  const decodedJwt = decode(jwtToken) as JwtPayload;
+  return decodedJwt.sub;
+}
+
+export function getToken(authHeader: string): string {
+  if (!authHeader) {
+    throw new Error('No authentication header');
+  }
+
+  if (!authHeader.toLowerCase().startsWith('bearer ')) {
+    throw new Error('Invalid authentication header');
+  }
+
+  const split = authHeader.split(' ');
+  const token = split[1];
+
+  return token;
 }
