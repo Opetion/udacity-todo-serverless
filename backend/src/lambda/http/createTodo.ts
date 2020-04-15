d8d47029672f365a9be36b86a100563ce78faa07
Opetion <jfc.costa91@gmail.com>
2020-04-15 14:53:32 +0100

    :sparkles: add TODO create

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,35 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import {parseUserIdHeader} from "../../auth/utils";
+import {createLogger} from "../../utils/logger";
+import {TodoRepository} from "../../repository/TodoRepository";
+import {TodoItem} from "../../models/TodoItem";
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+import * as middy from 'middy'
+import cors from '@middy/http-cors'
 
-  // TODO: Implement creating a new TODO item
-  return undefined
-}
+const repository: TodoRepository = new TodoRepository();
+const logger = createLogger("createTodo");
+
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const newTodo: CreateTodoRequest = JSON.parse(event.body);
+  const authHeader = event.headers["Authorization"];
+  const userId = parseUserIdHeader(authHeader);
+  logger.info(`Create TODO [${userId}]  with ${newTodo}`);
+  const result : TodoItem = await repository.create(newTodo, userId);
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      item:result
+    })
+  }
+});
+
+handler.use(cors());
