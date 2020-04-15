6916e3c1c5cb2a0e8568de79547a45902bb8da68
Opetion <jfc.costa91@gmail.com>
2020-04-15 14:54:47 +0100

    :sparkles: add TODO get all from user

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,36 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from "middy";
+import { parseUserIdHeader } from '../../auth/utils'
+import { TodoRepository } from '../../repository/TodoRepository'
+import { createLogger } from '../../utils/logger'
+import cors from '@middy/http-cors'
+import { AttachmentStorage } from '../../storage/AttachmentStorage'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
-}
+const repository: TodoRepository = new TodoRepository();
+const storage: AttachmentStorage = new AttachmentStorage();
+const logger = createLogger("indexTodos");
+
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const authHeader = event.headers["Authorization"];
+  const userId = parseUserIdHeader(authHeader);
+  const result = await repository.getAllFromUser(userId)
+  logger.info(`Index TODO [${userId}] with ${result}`);
+
+  for(const item of result){
+    if(!item.attachmentUrl){
+      continue;
+    }
+    item.attachmentUrl = await storage.getAttachment(item.todoId);
+  }
+
+  return {
+    statusCode:200,
+    body: JSON.stringify({
+      items:result
+    })
+  }
+});
+
+handler.use(cors());
