466e1e83003cf6cba1d14d07a095bcbded373110
Opetion <jfc.costa91@gmail.com>
2020-04-15 14:54:31 +0100

    :sparkles: add TODO S3 storage url generator

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,54 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda'
+import * as middy from "middy";
+import { TodoItem } from '../../models/TodoItem'
+import { TodoRepository } from '../../repository/TodoRepository'
+import { createLogger } from '../../utils/logger'
+import { parseUserIdHeader } from '../../auth/utils'
+import { AttachmentStorage } from '../../storage/AttachmentStorage'
+import cors from '@middy/http-cors'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const repository: TodoRepository = new TodoRepository();
+const storage: AttachmentStorage = new AttachmentStorage();
+const logger = createLogger("generateUrlTodo");
+
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
+  const item : TodoItem = await repository.find(todoId);
+  const authHeader = event.headers["Authorization"];
+  const userId = parseUserIdHeader(authHeader);
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
-}
+  if(!item){
+    return  {
+      statusCode: 404,
+      body: JSON.stringify({
+        "message":"item not found"
+      })
+    };
+  }
+
+  if(userId !== item.userId) {
+    return  {
+      statusCode: 403,
+      body: JSON.stringify({
+        "message":"No permissions on the item"
+      })
+    };
+  }
+
+  const url = await storage.getUploadUrl(item.todoId)
+  item.attachmentUrl=url;
+  await repository.update(item)
+
+  logger.info(`Generated url for ${todoId}`);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      uploadUrl:url
+    })
+  }
+});
+
+handler.use(cors());
